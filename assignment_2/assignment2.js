5f92a64ed9579bcae022c9fa1732832cc54c17db
Teanna Kimberley <teannakimberley@Teannas-MacBook-Pro.local>
2017-05-27 18:38:43 -0400

    assignment code push

--- assignment_2/assignment2.js
+++ assignment_2/assignment2.js
@@ -3,7 +3,10 @@ Exercise #1
 Create a for loop that prints out the numbers 1 to 100 in the console.
 */
 
-
+ for(var i=0;i<=100;i++){
+    myArray.push(i);
+    console.log([i]);
+ }
 
 /*
 Exercise #2
@@ -17,3 +20,10 @@ Write a loop that makes seven calls to console.log to output the following trian
 ######
 #######
 */
+var z = '';
+var x = '';
+var myArray = [1=="#",2=="##",3=="###",4=="####",5=="#####",6=="######",7=="#######"];
+ for(var i=1;i<=7;i++){z='';
+for(var i2=1;i2<i;i2++) { z=z+'#'; }
+    console.log(z);
+ }
