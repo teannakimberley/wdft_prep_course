54e4c17f1beece5e8bec689c651c38b2034df57b
Teanna Kimberley <teannakimberley@Teannas-MacBook-Pro.local>
2017-05-27 16:26:43 -0400

    second code push

---
+++ OPTIONALCHALLENGES.js
@@ -0,0 +1,53 @@
+/*
+Exercise #1
+Hello Name be your name
+*/
+var name = "Teanna";
+console.log ("Hello " + name);
+
+/*
+Excercise #2
+*/
+
+var x = 1125;
+var answer = x%2;
+console.log (answer)
+
+/*
+Excercise #3
+*/
+
+var a = true;
+var b = false;
+var c = true;
+console.log (a||b||c)
+
+/*
+Excercise #4
+*/
+var a = true;
+var b = false;
+var c = true;
+console.log (a&&b&&c)
+
+/*
+Excercise #5
+*/
+console.log (10 - 3 * 5/4 + 202 % 3);
+var answer = 1;
+console.log (10 - (3 * (5/4) + (202 % 3)));
+var answer = 2;
+
+/* 
+Excercise 6 
+*/
+var t = 12;
+var e = "113";
+var a = "1.12356";
+var x1 = "hello world";
+
+t2 = String(t);
+e2 = parseInt(e);
+a2 = parseFloat(a);
+x = parseInt(x1);
+console.log (parseInt("Hello world"));
